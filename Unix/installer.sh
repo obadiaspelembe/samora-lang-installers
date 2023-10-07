cd4067403d382f0f47a7845a2fc4dee6872501f0
Eusebio Simango <esimango@technoplus.co.mz>
2023-10-08 00:42:36 +0200

    feat: add unix installer

---
+++ Unix/installer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Check if go is installed
+if ! command_exists go; then
+  # Install Go
+  echo "Installing Go..."
+  sudo apt update
+  sudo apt install -y golang
+fi
+
+
+export GOPATH="$HOME/go"
+export PATH="$PATH:$GOPATH/bin"
+
+
+temp_dir=$(mktemp -d)
+echo "Temporary directory: $temp_dir"
+echo "cloning the Samora-Lang repository ..."
+
+git clone https://github.com/GraHms/Samora-Lang "$temp_dir"
+cd "$temp_dir"
+
+echo "Building..."
+go build -o "samora" main.go
+
+echo "Installing samora-lang"
+
+cp samora /usr/bin/samora
+
+
+echo "nstallation Completed."
+
+rm -rf "$temp_dir"
+
